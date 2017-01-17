136aa5103277dfeae82be5ccbf211782aef45acb
Erik Bernhardsson <erikbern@spotify.com>
2017-01-16 22:36:51 -0500

    vairous analyze.py fixes, still not done

--- analyze.py
+++ analyze.py
@@ -1,51 +1,16 @@
-import bisect, json, numpy, numpy.fft, os, random, requests, scipy.signal, sha, soundfile, subprocess, sys, tempfile
+import bisect, json, math, numpy, numpy.fft, os, pandas, random, requests, scipy.signal, sha, soundfile, subprocess, sys, tempfile, traceback
 from matplotlib import pyplot
-import matplotlib.mlab
 
 def freq_from_fft(signal, fs):
     # https://gist.github.com/endolith/255291/fb8794f0bc5d4ae98890fcbaa0af58e75f781993
-    N = len(signal)
-
     # Compute Fourier transform of windowed signal
-    windowed = signal * scipy.signal.kaiser(N, 100)
+    windowed = signal * scipy.signal.kaiser(len(signal), 14)
     f = numpy.fft.rfft(windowed)
-    hz_factor = 1.0 * fs / N
-    freqs = numpy.arange(len(f)) * hz_factor
-
-    #pyplot.plot(freqs[:300], abs(f[:300]))
-    #pyplot.show()
-
-    i = numpy.argmax(abs(f))
-    return freqs[i]
-    # return numpy.dot(freqs, abs(f)) / numpy.sum(abs(f))
+    hz_factor = 1.0 * fs / len(signal)
+    return f, hz_factor
 
 
-def freq_from_autocorr(sig, fs):
-    """
-    Estimate frequency using autocorrelation
-    """
-    # Calculate autocorrelation (same thing as convolution, but with
-    # one input reversed in time), and throw away the negative lags
-    corr = scipy.signal.fftconvolve(sig, sig[::-1], mode='full')
-    corr = corr[len(corr)//2:]
-
-    # Find the first low point
-    d = numpy.diff(corr)
-    x = matplotlib.mlab.find(d > 0)
-    if not len(x):
-        return
-    start = x[0]
-
-    # Find the next peak after the low point (other than 0 lag).  This bit is
-    # not reliable for long signals, due to the desired peak occurring between
-    # samples, and other peaks appearing higher.
-    # Should use a weighting function to de-emphasize the peaks at longer lags.
-    peak = numpy.argmax(corr[start:]) + start
-        
-    return float(fs) / peak
-
-
-def download(url):
+def download(url, fs=22050):
     fn_mp3 = 'clips/%s.mp3' % sha.new(url).hexdigest()
     fn_wav = 'clips/%s.wav' % sha.new(url).hexdigest()
     if not os.path.exists(fn_mp3):
@@ -56,20 +21,16 @@ def download(url):
         f.close()
     if not os.path.exists(fn_wav):
         print 'converting %s -> %s' % (fn_mp3, fn_wav)
-        subprocess.call('ffmpeg -y -i %s -vn -acodec pcm_s16le -ac 1 -ar 22050 -f wav %s' % (fn_mp3, fn_wav), shell=True)
+        subprocess.call('ffmpeg -y -i %s -vn -acodec pcm_s16le -ac 1 -ar %d -f wav %s' % (fn_mp3, fs, fn_wav), shell=True)
     return fn_wav
 
 
-def trim(sig, fs, s=0.5):
+def trim(sig, fs, s):
     samples = int(s * fs)
     c = numpy.cumsum(abs(sig))
     i = numpy.argmax(c[samples:] - c[:-samples])    
     lo, hi = i, i+samples
     return sig[lo:hi]
-
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    return b, a
 
 
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
@@ -84,22 +45,29 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
     return data
 
 
-def get_freq(fn):
+def get_signal(fn, trim_s=0.5):
     try:
         signal, fs = soundfile.read(fn)
-        signal = trim(signal, fs)
+        signal = butter_bandpass_filter(signal, 40., 500., fs)
+        signal = trim(signal, fs, trim_s)
+        assert len(signal) == int(fs * trim_s)
     except:
+        traceback.print_exc()
         return None
-    signal = butter_bandpass_filter(signal, 100., 250., fs)
-    return freq_from_autocorr(signal, fs)
+    return signal
 
 data = [json.loads(line.strip()) for line in open('clips.jsons')]
 data.sort(key=lambda l: sha.new(l['url']).hexdigest())
 
-f = open('clips_analyzed.jsons', 'w')
+f = open('clips_analyzed_2.jsons', 'w')
 lu2c = {} # (lang, user) to count
 urls = set()
-for l in data:
+fs = 22050
+trim_s = 2.0
+
+df = []
+
+for l in data[:1000]:
     if l['url'] in urls:
         continue
     urls.add(l['url'])
@@ -119,9 +87,19 @@ for l in data:
         origin = l['origin'].split(' from ')[1].rstrip(')')
     else:
         origin = None
+    lang = l['lang_code']
+
     fn = download(l['url'])
-    freq = get_freq(fn)
-    f.write(json.dumps({'fn': fn, 'freq': freq, 'gender': gender, 'lang_code': l['lang_code'], 'lang': l['lang'], 'origin': origin}) + '\n')
+    signal = get_signal(fn)
+    if signal is not None:
+        freqs, hz_factor = freq_from_fft(signal, fs)
+        df.append((lang, gender, origin, abs(freqs[:500])))
 
+df = pandas.DataFrame(df, columns=['lang', 'gender', 'origin', 'freqs'])
+print df
 
+for lang_code, f in base_freqs.iteritems():
+    pyplot.plot(numpy.arange(300) * hz_factor, f[:300] / numpy.max(f), label=lang_code)
+pyplot.legend()
+pyplot.show()
 
